693f47e54351961da0b2d4be2dab14c0114e556b
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-09-29 21:13:04 -0500

    controllers with parameters

--- hello-spring-demo/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring-demo/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -2,6 +2,8 @@ package org.launchcode.hello_spring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,6 +20,20 @@ public class HelloController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @GetMapping("hello/name")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 
 }

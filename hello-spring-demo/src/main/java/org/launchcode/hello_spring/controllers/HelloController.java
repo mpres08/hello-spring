1ee323658190f6ea7b050bab52c280c71554d1a5
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-09-29 21:41:14 -0500

    class annotations

--- hello-spring-demo/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ hello-spring-demo/src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -4,37 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     // Handles requests at path /hello
-    @GetMapping("hello")
-    @ResponseBody
     public String hello() {
         return "Hello, Spring!";
     }
 
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello/name")
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "name")
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     // Handles requests of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
